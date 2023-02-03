bb77814677c746e1b51c6cd41249676567153ef4
Roland Senn <rsx@bluewin.ch>
2023-02-03 14:18:07 +0100

    Add Solve_vu_unsafe

--- app/Main.hs
+++ app/Main.hs
@@ -11,6 +11,7 @@ import Solve_matrix
 import Solve_ll
 import Solve_lu
 import Solve_vu
+import Solve_vu_unsafe
 import Solve_mu
 import Solve_muf
 import Solve_rosetta1
@@ -31,29 +32,31 @@ main = do
   putStrLn $ show ex3rosettaMatrix
   putStrLn $ show ex3rosettaRight
 
-{-
   defaultMain [
     bgroup "3*3"
-                 [ bench "matrix "   $ nf solve_matrix ex1data_matrix
-                 , bench "rosetta code example1 "   $ nf (gauss ex1rosettaMatrix) ex1rosettaRight
-                 , bench "list of lists "   $ nf solve_ll ex1data_ll
+                 [ --bench "matrix "   $ nf solve_matrix ex1data_matrix
+                 -- , bench "rosetta code example1 "   $ nf (gauss ex1rosettaMatrix) ex1rosettaRight
+                 bench "list of lists "   $ nf solve_ll ex1data_ll
                  , bench "list of unboxed vectors" $ nf solve_lu ex1data_lu
                  , bench "vector of unboxed vectors" $ nf solve_vu ex1data_vu
+                 , bench "vector of unboxed vectors(unsafe)" $ nf solve_vu_unsafe ex1data_vu
                  , bench "mixed (list/vector) (state) of unboxed vectors" $ nf solve_mu ex1data_vu
                  , bench "mixed (list/vector) (fold) of unboxed vectors" $ nf solve_muf ex1data_vu
                  ]
     ]
--}
+{-
 
 
   defaultMain [
     bgroup "10*10"
-                 [ bench "matrix "   $ nf solve_matrix ex3data_matrix
-                 , bench "rosetta code example1 "   $ nf (gauss ex3rosettaMatrix) ex3rosettaRight
-                 , bench "list of lists "   $ nf solve_ll ex3data_ll
+                 [ -- bench "matrix "   $ nf solve_matrix ex3data_matrix
+                 --, bench "rosetta code example1 "   $ nf (gauss ex3rosettaMatrix) ex3rosettaRight
+                 bench "list of lists "   $ nf solve_ll ex3data_ll
                  , bench "list of unboxed vectors" $ nf solve_lu ex3data_lu
                  , bench "vector of unboxed vectors" $ nf solve_vu ex3data_vu
+                 , bench "vector of unboxed vectors (unsafe)" $ nf solve_vu_unsafe ex3data_vu
                  , bench "mixed (list/vector) of unboxed vectors (state)" $ nf solve_mu ex3data_vu
                  , bench "mixed (list/vector) of unboxed vectors (fold)" $ nf solve_muf ex3data_vu
                  ]
     ]
+-}
---
+++ src/Solve_vu_unsafe.hs
@@ -0,0 +1,76 @@
+{-# Language OverloadedStrings #-}
+
+module Solve_vu_unsafe (solve_vu_unsafe) where
+
+import qualified Data.Text as T
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as VU
+import Control.Monad.Trans.State.Strict
+
+type Equation = VU.Vector Double
+type Matrix = V.Vector Equation
+
+cLIMIT :: Double
+cLIMIT = 0.001
+
+cINVALID :: T.Text
+cINVALID = "Attempt to invert a non-invertible matrix"
+
+
+
+solve_vu_unsafe :: Matrix -> Either T.Text (VU.Vector Double)
+solve_vu_unsafe mat = calcTriangle mat >>= backInsert
+
+calcTriangle :: Matrix -> Either T.Text (V.Vector Equation, Matrix)
+calcTriangle mat = runStateT (sequence (StateT . pivotStep <$> ops)) mat
+    where
+        ops = V.enumFromN 1 $ pred $ length mat
+
+pivotStep :: Int -> Matrix -> Either T.Text (Equation, Matrix)
+pivotStep _ mat0 = do
+    let (rowp, mat) = getNextPivot mat0
+        pivot = VU.unsafeHead rowp
+    if  abs pivot < cLIMIT
+      then Left cINVALID
+      else Right $ (rowp, V.map (newRow rowp) mat)
+
+-- Find biggest pivot in first column.
+-- Remove row with pivot from matrix
+-- Return pivot and new matrix
+getNextPivot :: Matrix -> (Equation, Matrix)
+getNextPivot mat =
+    let len = V.length mat
+        ixrow = snd $ maximum $ V.zip (V.map (abs . VU.unsafeHead) mat) (V.enumFromN 0 (len - 1))
+        strtRows = V.unsafeSlice 0 ixrow mat
+        rowp = V.unsafeIndex mat ixrow
+        endRows = V.unsafeSlice (ixrow + 1) (len - ixrow -1) mat
+    in  (rowp, strtRows <> endRows)
+
+-- Apply the pivot to a row
+newRow :: Equation -> Equation -> Equation
+newRow rowp row = VU.unsafeTail $
+                   VU.zipWith (+)
+                     (applyPivot rowp row)
+                     row
+
+applyPivot :: Equation -> Equation -> Equation
+applyPivot rowp row = VU.map (f *) rowp
+   where
+    f = negate $ VU.unsafeHead row / VU.unsafeHead rowp    -- Works only if pivot is in the first column
+
+backInsert :: (V.Vector Equation, Matrix) -> Either T.Text (VU.Vector Double)
+backInsert (eqs , ress) = do
+    let res = V.unsafeHead ress
+        piv = VU.unsafeHead res
+        val = VU.unsafeLast res
+        xn  = val / piv
+    if  abs piv < cLIMIT
+      then Left cINVALID
+      else Right $ V.foldr stepInsert (VU.singleton xn) eqs
+
+stepInsert :: Equation -> VU.Vector Double ->  VU.Vector Double
+stepInsert equat xs =
+   let piv = VU.unsafeHead equat
+       as = (VU.unsafeTail . VU.unsafeInit) equat
+       s = VU.unsafeLast equat - (VU.sum  $ VU.zipWith (*) as (xs))
+    in VU.cons (s/piv)  xs
