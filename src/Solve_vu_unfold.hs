0f1cb9f11cd139f5a4ce7c8adc51a2460d7d24a8
Roland Senn <rsx@bluewin.ch>
2023-04-25 15:35:58 +0200

    Add optimizations from state also to unfold

--- src/Solve_vu_unfold.hs
+++ src/Solve_vu_unfold.hs
@@ -5,8 +5,8 @@ module Solve_vu_unfold (solve_vu_unfold) where
 import qualified Data.Text as T
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as VU
-
-import BenchmarkData
+import qualified VectorBuilder.Builder as B
+import qualified VectorBuilder.Vector as C
 
 type Equation = VU.Vector Double
 type Matrix = V.Vector Equation
@@ -29,7 +29,7 @@ checkMatrix mat = checkMatrixSameLength mat >>= checkRowsCols
 checkMatrixSameLength :: Matrix -> Either T.Text Matrix
 checkMatrixSameLength mat = do
     let length1 = VU.length $ V.head mat
-    if all (\r -> (VU.length r == length1)) mat
+    if all (\r -> VU.length r == length1) mat
         then Right mat
         else Left " Not all equations have the same length"
 
@@ -45,51 +45,35 @@ checkRowsCols mat = do
 -- Calculation
 -- -------------------------------------------------------------------
 calcTriangle :: Matrix -> Either T.Text (V.Vector Equation)
-calcTriangle mat= V.unfoldrExactNM (V.length mat) pivotStep mat
-
-pivotStep :: Matrix -> Either T.Text (Equation, Matrix)
-pivotStep  mat =
-    if abs negPivot < cLIMIT
-      then Left cNONSOLVABLE
-      else Right (rowp, V.map newRow newMat)
+calcTriangle mat0 = V.unfoldrExactNM (V.length mat0) pivotStep mat0
   where
-    (rowp, newMat) = getNextPivot mat
-    tailrowp = VU.tail rowp
-    negPivot = negate $ VU.head rowp
-    -- Apply the pivot to a row
-    newRow :: Equation -> Equation
-    newRow row = VU.zipWith (+)
+    pivotStep :: Matrix -> Either T.Text (Equation, Matrix)
+    pivotStep mat =
+        if abs negPivot < cLIMIT
+        then Left cNONSOLVABLE
+        else Right (pivotrow, V.map newRow newMat)
+      where
+        ixprow = snd $ maximum $ V.imap (\ix e -> ((abs . VU.head) e, ix)) mat
+        pivotrow = (V.!) mat ixprow
+        -- newMat = V.ifilter (\ix _ -> ix /= ixprow) mat
+        newMat = V.imapMaybe ixFilter mat   -- This is faster than V.ifilter !!
+          where
+            ixFilter :: Int -> a -> Maybe a
+            ixFilter ix v
+                | ix == ixprow = Nothing
+                | otherwise  = Just v
+
+        -- Apply the pivot to a row
+        newRow :: Equation -> Equation
+        newRow row = VU.zipWith (+)
                      (applyPivot (VU.head row))
                      (VU.tail row)
-    applyPivot :: Double -> Equation
-    applyPivot hdRow = VU.map (hdRow / negPivot *) tailrowp
 
--- Find biggest pivot in first column.
--- Remove row with pivot from matrix
--- Return pivot row and new matrix
-getNextPivot :: Matrix -> (Equation, Matrix)
-getNextPivot mat
-    | len > 1 = (rowp, newMat)
-    | otherwise = (V.head mat, V.empty)
-  where
-    len = V.length mat
-    -- Get the index of the row with biggest pivot
-    -- Versions with elemIndex or maxIndexBy are much slower
-    -- absvect = V.map (abs . VU.head) mat
-    -- ixabs = V.map swap $ V.indexed absvect
-    -- ixrow = snd $ V.maximum ixabs
-    -- ixrow = V.maxIndexBy (\x y -> compare (abshead x) (abshead y)) mat
-    -- abshead = abs . VU.head
-    ixrow = snd $ V.maximum $ V.zip (V.map (abs . VU.head) mat) (V.enumFromN 0 len)
-    (rowp, newMat) = if ixrow > 0
-        then  let r0 = V.head mat
-                  rix = mat V.! ixrow
-                  lst = [(0,rix), (ixrow,r0)]
-                  swapMat = (V.//) mat lst
-              in  (rix, V.tail swapMat)
-        else  (V.head mat, V.tail mat)
-    -- rowp = V.head endRows
-
+        applyPivot :: Double -> Equation
+        applyPivot hdRow = VU.map (hdRow / negPivot *) tailprow
+        -- The next 2 values do not change between rows in applyPivot!
+        tailprow = VU.tail pivotrow
+        negPivot = negate $ VU.head pivotrow
 
 backInsert :: V.Vector Equation -> Either T.Text (VU.Vector Double)
 backInsert eqs = Right $ V.foldr stepInsert VU.empty eqs
@@ -99,7 +83,9 @@ backInsert eqs = Right $ V.foldr stepInsert VU.empty eqs
         let piv = VU.head equat
             as = (VU.tail . VU.init) equat
             s = VU.last equat - VU.sum (VU.zipWith (*) as xs)
-        in VU.cons (s/piv)  xs
+        in cons (s/piv) xs
+    cons :: Double -> VU.Vector Double -> VU.Vector Double
+    cons el vect = C.build builder
+      where
+        builder = B.singleton el <> B.vector vect
 
-main :: IO ()
-main = print $ solve_vu_unfold ex1data_vu
